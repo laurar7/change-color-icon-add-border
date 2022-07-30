1bc6fcedec1c02241000d5c46ac4b94fcda3ab0c
Laura R <102334514+laurar7@users.noreply.github.com>
2022-07-30 16:03:37 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+// console.log(instagram);
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+// console.log(facebook);
+
+var contact = document.querySelector(".contact p");
+contact.style.border = "1px solid #d45bb9";
+contact.style.padding = "1em";
+// console.log(contact);
